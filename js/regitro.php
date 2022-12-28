c8a955e5cd6167efd6c28a9fe74579197551d7fa
geovannysan <geovanny1santiago@gmail.com>
2022-12-28 17:16:55 -0500

    nuevo registro

--- js/regitro.php
+++ js/regitro.php
@@ -4,31 +4,38 @@ include('conexion.php');
 header("Content-Type: application/json");
 $data = array();
 
-    $username = $_POST['nombre'];
+    $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $manz = $_POST['manz'];
     $vill = $_POST['vill'];
     $Cedular = $_POST['Cedular'];
     $Correo = $_POST['Correo'];
-    $query = $connection->prepare("INSERT INTO usurarios(Nombre,Apellido,MZ,Villas,Celular,Correo) VALUES (:username,:apellido,:manz,:vill,:Cedular,:Correo)");
-        $query->bindParam("nombre", $username, PDO::PARAM_STR);
+    if(empty( $nombre) && empty($apellido)&& empty($manz) &&  empty($Cedular) && empty($Correo) && empty($vill)){
+        $data['status'] = false;
+        $data['result'] = "complete los datos";
+     
+        echo json_encode($data);
+    }
+    else{
+    $query = $connection->prepare("INSERT INTO usurarios(Nombre,Apellido,MZ,Villas,Celular,Correo) VALUES (:nombre,:apellido,:manz,:vill,:Cedular,:Correo)");
+        $query->bindParam("nombre", $nombre, PDO::PARAM_STR);
         $query->bindParam("apellido", $apellido, PDO::PARAM_STR);
-        $query->bindParam("manz", $manz, PDO::PARAM_STR);
-        $query->bindParam("vill", $vill, PDO::PARAM_STR);
-        $query->bindParam("Cedular", $Cedular, PDO::PARAM_STR);
-        $query->bindParam("Correo", $Correo, PDO::PARAM_STR);
+        $query->bindParam("manz", $manz, PDO::PARAM_INT );
+        $query->bindParam("vill", $vill, PDO::PARAM_INT );
+        $query->bindParam("Cedular", $Cedular, PDO::PARAM_INT );
+        $query->bindParam("Correo", $Correo, PDO::PARAM_STR );
         $result = $query->execute();
         if ($result) {
             $data['status'] = true;
-            $data['result'] = $result;
+            $data['result'] = "Usuario registrado";
          
             echo json_encode($data);
         } else {
-            $data['status'] = true;
-            $data['result'] = $result;
+            $data['status'] = false;
+            $data['result'] =  "No se completo el registro";
          
             echo json_encode($data);
         }
-
+}
 
 ?>
