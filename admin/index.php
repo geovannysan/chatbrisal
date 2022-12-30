5e01d5973bf923c1a1754c0b814eac6839fa6b3f
geovannysan <geovanny1santiago@gmail.com>
2022-12-30 17:43:14 -0500

    nuevios

--- admin/index.php
+++ admin/index.php
@@ -37,7 +37,8 @@ $query4 = $bd->execute("select * from usurarios ");
 <link rel="stylesheet" href="../css/animate.css">
 <link rel="stylesheet" href="../css/fontello.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
+<script src="https://jsuites.net/v4/jsuites.js"></script>
+  <link rel="stylesheet" href="https://jsuites.net/v4/jsuites.css" type="text/css" />
 
 <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.3.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
@@ -48,6 +49,9 @@ $query4 = $bd->execute("select * from usurarios ");
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap4.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.0/css/responsive.bootstrap4.min.css">
 
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg  " style="font-size: 1.1rem; background: #0e385e">
